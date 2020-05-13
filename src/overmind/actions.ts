ac0008860c7710d1b8c8eaeba74361678f544fa4
Geir Sagberg <geir.sagberg@gmail.com>
2020-05-13 13:39:24 +0200

    Minimum reproducible

--- src/overmind/actions.ts
+++ src/overmind/actions.ts
@@ -4,7 +4,8 @@ export const addUser: AsyncAction = async ({
   state: { users },
   effects: { saveUser },
 }) => {
-  const userName = prompt('Brukernavn')
+  // const userName = prompt('Brukernavn')
+  const userName = 'lol'
   if (userName) {
     const id = await saveUser({ userName })
     users[id] = { id, userName }
--- src/overmind/effects.ts
+++ src/overmind/effects.ts
@@ -1,13 +1,14 @@
-import Dexie from 'dexie'
+// import Dexie from 'dexie'
 
-const db = new Dexie('auth-man')
-db.version(1).stores({
-  users: '++id, userName',
-  roles: '++id, name',
-  permissions: '++id, operations, resources',
-})
+// const db = new Dexie('auth-man')
+// db.version(1).stores({
+//   users: '++id, userName',
+//   roles: '++id, name',
+//   permissions: '++id, operations, resources',
+// })
 
 export const saveUser = async ({ userName }: { userName: string }) => {
-  const id = await db.table('users').add({ userName })
+  // const id = await db.table('users').add({ userName })
+  const id = 3
   return id.toString()
 }
